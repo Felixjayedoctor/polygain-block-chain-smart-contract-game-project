a05a8457703d97362785597d1dab8609b141f23c
Your Name <you@example.com>
2022-11-26 18:05:50 +0100

    first commit

---
+++ scriptform.js
@@ -0,0 +1,136 @@
+// while(number < 10){
+//     alert("number is less than 10")
+// }
+
+// for (let i=0; i<3; i){
+//     alert("i love you")
+// }
+   
+
+
+
+// if(gender = "female"){
+//     alert("you are a lady")
+// }
+// else if(gender = "male"){
+//     ("you are a fuy")
+// }
+// else{
+//     alert("i dont know what you are")
+// }
+
+// function showMessage(){
+//     alert("hi there")
+// }
+
+// showMessage()
+
+
+// let user = {}
+//  user["name"] = "i am victoria"
+//  console.log
+
+
+// username//
+function getUserDetails(){
+    let UserName = prompt("enter your username")
+    if (UserName == null){
+        return
+    }
+
+
+    function validateUserName(UserName){
+        if(UserName.length < 10 & UserName.length > 0){
+            return true
+        }else{
+            return false
+        }
+    }
+    
+    while(validateUserName(UserName) == false)(
+        UserName = prompt("Username must be less than 10 but greater than 0")
+    )
+
+
+
+    //phonenumber//
+    let PhoneNumber = prompt("enter your phone number")
+    function validatephonenumber(PhoneNumber){
+        if(PhoneNumber.length == 11){
+            return true
+        }else{
+            return false
+        }
+    }
+    
+while(validatephonenumber(PhoneNumber) == false)(
+    PhoneNumber = prompt("phone number must be 11 digits, try again!")
+)
+if ((PhoneNumber) == null){
+    return
+}
+
+
+    //email address//
+    let Email = prompt("enter your email")
+    function validateEmail(Email){
+        if (Email.length > 7){
+            return true
+        }else{
+            return false
+        }
+    }
+    console.log(validateEmail(Email))
+    
+    let password = prompt("enter your password")
+    function validatepassword(password){
+        if(password.length > 7){
+            return true
+        }else{
+            return false
+        }
+    }
+    while(validateEmail(Email) == false)(
+        Email = prompt("email address does not match!. Try again")
+    )
+    
+    let ConfirmPassword = prompt("confirm your password")
+    function validateConfirmPassword(ConfirmPassword){
+        if(ConfirmPassword != password){
+            return false
+        }else{
+            return true
+        }
+    }
+    console.log(validateConfirmPassword(ConfirmPassword))
+    
+    let goway = prompt("enter now")
+    function validategoway(goway){
+        if(goway.length > 5){
+            return true
+        }else{
+            return false
+        }
+    }
+    console.log(validategoway(goway))
+    
+    let myman = prompt("come inside please")
+    function validatemyman(myman){
+        if(myman.length = 10){
+            return true
+        }else{
+            return false
+        }
+    }
+    console.log(validatemyman(myman))
+    
+    let Hit = prompt("Hit hard")
+    function validatehit(Hit){
+        if(Hit.length <2){
+            return true
+        }else{return false
+        }
+    }
+    console.log(validatehit(Hit))
+}
+
